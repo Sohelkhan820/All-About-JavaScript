90e52f7f2a6165c98afd3aead852c2617d8c9231
Sohel Khan <133994864+Sohelkhan820@users.noreply.github.com>
2023-12-19 18:29:56 +0600

    Add files via upload

---
+++ Day 10/index.js
@@ -0,0 +1,6 @@
+var fer = parseFloat(prompt ("Enter your ferenthut :"));
+
+var cels = (fer-32) * (5/9);
+
+
+document.write("celsius = " + cels);
---
+++ Day 11/index.js
@@ -0,0 +1,30 @@
+// Relational opertor ->  >, >=, <, <=, ==, ===, !=, !==.
+// logical operator -> &&, ||
+
+// var num1 = 30;
+// var num2 = 40;
+// var num3 = 60;
+
+// console.log(num1>num2);
+// console.log(num1<num2);
+// console.log(num1==num2);
+// console.log(num1<=num2);
+// console.log(num1!==num2);
+
+// console.log(num1<num2 && num1<num3);
+// console.log(num1<num2 && num3<num1);
+
+
+// console.log(num2<num1 || num3<num1);
+// console.log(num2<num1 || num2<num3);
+
+
+// console.log(num2 != num1);
+// console.log(num1 != num2);
+
+// console.log(! (20>14));
+// console.log(! (20<14));
+
+var num1 =parseFloat(prompt)("enter your number");
+var num1 =80;
+if 
